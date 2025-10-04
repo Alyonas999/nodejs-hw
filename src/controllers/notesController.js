86a84c2240a74965f42c9dcade6bb2ba8c171089
Alyona <oranchuka@gmail.com>
2025-10-04 19:39:35 +0300

    server.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,53 +1,63 @@
-import createHttpError from 'http-errors';
-import { Notes } from '../models/note.js';
-
-export const getAllNotes = async (req, res) => {
-  const notes = await Notes.find();
-  res.status(200).json(notes);
-};
+import createHttpError from "http-errors";
+import { Note } from "../models/note.js";
 
 
-export const getNotesById = async (req, res, next) => {
-  const { notesId } = req.params;
-  const notes = await Notes.findById(notesId);
-
-  if (!notes) {
-    next(createHttpError(404, 'Note not found'));
-    return;
+export const getAllNotes = async (req, res, next) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (err) {
+    next(createHttpError(500, err.message));
   }
-
-  res.status(200).json(notes);
 };
 
-export const createNotes = async (req, res) => {
-  const notes = await Notes.create(req.body);
-  res.status(201).json(notes);
-};
 
-export const deleteNotes = async (req, res, next) => {
-  const { notesId } = req.params;
-  const notes = await Notes.findOneAndDelete({
-    _id: notesId,
-  });
-  if (!notes) {
-    next(createHttpError(404, "Notes not found"));
-    return;
+export const getNoteById = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const note = await Note.findById(noteId);
+    if (!note) {
+      return next(createHttpError(404, "Note not found"));
+    }
+    res.status(200).json(note);
+  } catch (err) {
+    next(createHttpError(500, err.message));
   }
-  res.status(200).send(notes);
 };
 
-export const updateNotes = async (req, res, next) => {
-  const { notesId } = req.params;
-  const notes = await Notes.findOneAndUpdate(
-    { _id: notesId },
-    req.body,
-    { new: true },
-  );
-
-  if (!notes) {
-    next(createHttpError(404, 'Notes not found'));
-    return;
+export const createNote = async (req, res, next) => {
+  try {
+    const note = await Note.create(req.body);
+    res.status(201).json(note);
+  } catch (err) {
+    next(createHttpError(500, err.message));
   }
+};
 
-  res.status(200).json(notes);
+export const updateNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+      new: true,
+    });
+    if (!note) {
+      return next(createHttpError(404, "Note not found"));
+    }
+    res.status(200).json(note);
+  } catch (err) {
+    next(createHttpError(500, err.message));
+  }
+};
+
+export const deleteNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const note = await Note.findOneAndDelete({ _id: noteId });
+    if (!note) {
+      return next(createHttpError(404, "Note not found"));
+    }
+    res.status(200).json(note);
+  } catch (err) {
+    next(createHttpError(500, err.message));
+  }
 };
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -10,6 +10,6 @@ export const errorHandler = (err, req, res, next) => {
 
   console.error(err);
   res.status(500).json({
-    message: err.message || "Simulated server error"
+    message: "Simulated server error", 
   });
 };
