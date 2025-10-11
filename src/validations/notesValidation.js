d68345fec1ab3dfb7fb6682d2ab8b519b23e5f36
Alyona <oranchuka@gmail.com>
2025-10-12 00:12:41 +0300

    notesValidation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -7,23 +7,24 @@ const objectIdValidator = (value, helpers) => {
 };
 
 export const getNotesQuerySchema = {
-  [Segments.QUERY]: {
+  [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(15).default(10),
-    tag: Joi.string().valid('Work',
-  'Personal',
-  'Meeting',
-  'Shopping',
-  'Ideas',
-  'Travel',
-  'Finance',
-  'Health',
-  'Important',
-      'Todo'),
+    tag: Joi.string().valid(
+      'Work',
+      'Personal',
+      'Meeting',
+      'Shopping',
+      'Ideas',
+      'Travel',
+      'Finance',
+      'Health',
+      'Important',
+      'Todo'
+    ),
     search: Joi.string().trim().allow("")
-  },
+  }),
 };
-
 
 /*get*/
 export const noteIdParamSchema = {
@@ -34,10 +35,10 @@ export const noteIdParamSchema = {
 
 /*post*/
 export const createNoteSchema = {
-  [Segments.BODY]: Joi.object()({
+  [Segments.BODY]: Joi.object({
     title: Joi.string().max(100).required(),
     content: Joi.string().max(1000).required(),
-    tag: Joi.string()  (
+    tag: Joi.string().valid(
       "Work",
       "Personal",
       "Meeting",
@@ -57,10 +58,10 @@ export const updateNoteSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom(objectIdValidator).required(),
   }),
-  [Segments.BODY]: Joi.object()({
+  [Segments.BODY]: Joi.object({
     title: Joi.string().max(100),
     content: Joi.string().max(1000),
-    tag: Joi.string() (
+    tag: Joi.string().valid(
       "Work",
       "Personal",
       "Meeting",
@@ -74,4 +75,3 @@ export const updateNoteSchema = {
     ),
   }).min(1),
 };
-
