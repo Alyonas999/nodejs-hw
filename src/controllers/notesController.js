eeab20f4cde5445443b95d420a39aced980ad524
Alyona <oranchuka@gmail.com>
2025-10-15 23:55:30 +0300

    fix-errors

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,49 +1,48 @@
 import createHttpError from "http-errors";
 import { Note } from "../models/note.js";
 
-
 export const getAllNotes = async (req, res, next) => {
+  try {
+    const {
+      page = 1,
+      perPage = 10,
+      tag,
+      search
+    } = req.query;
 
-  const {
-    page = 1,
-    perPage = 10,
-    tag,
-    search
-  } = req.query;
+    const skip = (page - 1) * perPage;
+    const notesQuery = Note.find({ userId: req.user._id });
 
-  const skip = (page - 1) * perPage;
-  const notesQuery = Note.find({ userId: req.user._id});
+    if (tag) {
+      notesQuery.where("tag").equals(tag);
+    }
+    if (search) {
+      notesQuery.where({
+        $text: { $search: search },
+      });
+    }
 
-   if (tag) {
-    notesQuery.where("tag").equals(tag);
-  }
-  if (search) {
-    notesQuery.where({
-      $text: { $search: search },
+    const [totalNotes, notes] = await Promise.all([
+      notesQuery.clone().countDocuments(),
+      notesQuery.skip(skip).limit(perPage),
+    ]);
+
+    const totalPages = Math.ceil(totalNotes / perPage);
+
+    res.status(200).json({
+      page,
+      perPage,
+      totalNotes,
+      totalPages,
+      notes,
     });
+    
+  } catch (err) {
+    next(createHttpError(500, err.message));
   }
-
-
-
-const [totalNotes, notes] = await Promise.all([
-   notesQuery.clone().countDocuments(),
-    notesQuery.skip(skip).limit(perPage),
-]);
-
-  const totalPages = Math.ceil(totalNotes / perPage);
-
-res.status(200).json({
-  page,
-  perPage,
-  totalNotes,
-  totalPages,
-  notes,
-  });
-
 };
 
 export const getNoteById = async (req, res, next) => {
-
   try {
     const { noteId } = req.params;
 
@@ -68,7 +67,9 @@ export const createNote = async (req, res, next) => {
       ...req.body,
       userId: req.user._id,
     });
+
     res.status(201).json(note);
+
   } catch (err) {
     next(createHttpError(500, err.message));
   }
@@ -82,21 +83,20 @@ export const updateNote = async (req, res, next) => {
       _id: noteId,
       userId: req.user._id,
     },
-      req.body, { new: true, });
+    req.body, { new: true });
 
     if (!note) {
       return next(createHttpError(404, "Note not found"));
     }
 
     res.status(200).json(note);
-    
+
   } catch (err) {
     next(createHttpError(500, err.message));
   }
 };
 
 export const deleteNote = async (req, res, next) => {
-
   try {
     const { noteId } = req.params;
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -14,9 +14,9 @@ import {
  } from "../validations/authValidation.js";
 
 const router = Router();
-router.post('/.auth/register', celebrate(registerUserSchema), registerUser);
-router.post('/.auth/login', celebrate(loginUserSchema), loginUser);
-router.post('/.auth/logout', logoutUser);
-router.post('/.auth/refresh', refreshUserSession);
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
