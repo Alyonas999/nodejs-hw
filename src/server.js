566debc51a16dab7e2445cedd915a688935be495
Alyona <oranchuka@gmail.com>
2025-11-18 17:36:47 +0200

    server

--- src/server.js
+++ src/server.js
@@ -10,17 +10,15 @@ import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
 import notesRoutes from "./routes/notesRoutes.js";
+
 const app = express();
 
-app.use(express.json({limit: '100kb',}));
+app.use(express.json());
 app.use(cors());
-app.use(cookieParser());
 app.use(logger);
 
-app.use("/", notesRoutes);
-
+app.use("/notes", notesRoutes);
 app.use(notFoundHandler);
-
 app.use(errorHandler);
 
 const startServer = async () => {
